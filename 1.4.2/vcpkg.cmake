25984dbe30d46a805c6a50603bec615d7794cab1
mathijs727 <mathijs.l.molenaar@gmail.com>
2020-04-24 12:36:03 +0200

    Revert "More disabling of metrics (duplicate code)"
    This reverts commit 6baf597cade5b7dd7c8044b27ff338de849796b6.

--- 1.4.2/vcpkg.cmake
+++ 1.4.2/vcpkg.cmake
@@ -176,7 +176,7 @@ function(_pmm_vcpkg)
         set(cmd ${CMAKE_COMMAND} -E env
                 CC=${CMAKE_C_COMPILER}
                 CXX=${CMAKE_CXX_COMPILER}
-            "${PMM_VCPKG_EXECUTABLE}" install -disableMetrics
+            "${PMM_VCPKG_EXECUTABLE}" install
                 --triplet "${ARG_TRIPLET}"
                 ${ARG_REQUIRES}
             )
